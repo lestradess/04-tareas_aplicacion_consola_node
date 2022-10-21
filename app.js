83bc963a6e55b441a9583e058ed637898535e36b
Lestrades_ubu <Lestrades@gmail.com>
2022-10-21 18:29:57 +0200

    Repetir menu de forma infinita

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ console.clear();
 const main = async() => {
 
     console.log("hola mundo");
+
+    let opt = ""
+
+    do {
+        opt = await mostrarMenu();
+        console.log({opt});
+        if (opt !== "0") await pausa();
+    } while (opt !== "0");
+
     mostrarMenu();
     pausa();
 
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,37 +1,47 @@
 require("colors");
 
 const mostrarMenu = async () => {
-    console.clear()
-    console.log("==================================".green);
-    console.log("    Selecione una opción".green);
-    console.log("==================================\n".green);
 
-    console.log(`${ "1.".green } Crear tarea`);
-    console.log(`${ "2.".green } Listar tareas`);
-    console.log(`${ "3.".green } Listar tareas completadas`);
-    console.log(`${ "4.".green } Listar tareas pendientes`);
-    console.log(`${ "5.".green } Completar tarea`);
-    console.log(`${ "6.".green } Borrar tarea`);
-    console.log(`${ "0.".green } Salir`);
+    return new Promise(resolve => {
 
-    //Salida y entrad de datos desde consola
-    const readLine = require("readline").createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
+        console.clear()
+        console.log("==================================".green);
+        console.log("    Selecione una opción".green);
+        console.log("==================================\n".green);
 
-    readLine.question("Seleccione una opción:", (opt) => {
-        readLine.close();
+        console.log(`${ "1.".green } Crear tarea`);
+        console.log(`${ "2.".green } Listar tareas`);
+        console.log(`${ "3.".green } Listar tareas completadas`);
+        console.log(`${ "4.".green } Listar tareas pendientes`);
+        console.log(`${ "5.".green } Completar tarea`);
+        console.log(`${ "6.".green } Borrar tarea`);
+        console.log(`${ "0.".green } Salir`);
+
+        //Salida y entrad de datos desde consola
+        const readLine = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        readLine.question("Seleccione una opción:", (opt) => {
+            readLine.close();
+            resolve(opt);
+        })
     })
+
 }
 const pausa = () => {
-    const readLine = require("readline").createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    readLine.question(`\nPresione ${ "Enter".green } para continuar\n`, (opt) => {
-        readLine.close();
+    return new Promise(resolve => {
+        const readLine = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        readLine.question(`\nPresione ${ "Enter".green } para continuar\n`, (opt) => {
+            readLine.close();
+            resolve();
+        })
     })
+
 }
 
 
