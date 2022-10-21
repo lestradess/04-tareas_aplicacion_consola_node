11983dfb810da8728f89c26df62a0e312c0d320b
Lestrad <lestrades@gmail.com>
2022-10-21 18:11:44 +0200

    Mostrar menu

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require("colors");
+const { mostrarMenu, pausa } = require("./helpers/mensajes");
+
+
 console.clear();
 
 const main = async() => {
 
     console.log("hola mundo");
+    mostrarMenu();
+    pausa();
 
 }
 main();
